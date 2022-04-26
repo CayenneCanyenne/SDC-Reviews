09072053be2135cc112c45d30004b805fc7485d1
jp-chang <rocchang.jp@gmail.com>
2022-04-26 15:57:00 -0400

    update DB info

--- database/index.js
+++ database/index.js
@@ -3,13 +3,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const {
-  DB_HOST, DB_PORT, DB_USER, DB_NAME,
+  DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASSWORD,
 } = process.env;
 
 const pool = new Pool({
   host: DB_HOST,
   port: DB_PORT,
   user: DB_USER,
+  password: DB_PASSWORD,
   database: DB_NAME,
 });
 
