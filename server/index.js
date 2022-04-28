d52fb3ccf2335d670d38eeb8dfc064e2ef80c1f4
jp-chang <rocchang.jp@gmail.com>
2022-04-28 14:10:16 -0400

    add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const cors = require('cors');
 const controller = require('./controllers');
 
 require('../database/index');
@@ -10,6 +11,7 @@ require('dotenv').config();
 const { PORT } = process.env;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
